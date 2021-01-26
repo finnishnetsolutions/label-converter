32037e3570b17e8bd3794c2d28c4645a4e30d747
Julius Leppälä <julius.leppala@fns.fi>
2021-01-26 10:16:52 +0300

    Fix trailing whitespaces

--- tests/test.py
+++ tests/test.py
@@ -7,18 +7,18 @@ HEADER = """<style>p{font-weight: boldest;}</style><p><strong>
 <span style="font-size: 18px;">&nbsp;Linda Svensson</span>
 &nbsp;</strong></p>"""
 BODY = """<p>Dalhemsvagen 26
-<br style="color: rgb(0, 0, 0); font-family: Arial; 
-font-size: medium; font-style: normal; font-variant-ligatures: normal; 
-font-variant-caps: normal; font-weight: 400; letter-spacing: normal; 
-orphans: 2; text-align: start; text-indent: 0px; text-transform: none; 
-white-space: normal; widows: 2; word-spacing: 0px; 
--webkit-text-stroke- text-decoration-thickness: initial; 
+<br style="color: rgb(0, 0, 0); font-family: Arial;
+font-size: medium; font-style: normal; font-variant-ligatures: normal;
+font-variant-caps: normal; font-weight: 400; letter-spacing: normal;
+orphans: 2; text-align: start; text-indent: 0px; text-transform: none;
+white-space: normal; widows: 2; word-spacing: 0px;
+-webkit-text-stroke- text-decoration-thickness: initial;
 text-decoration-style: initial; text-decoration-color: initial;">
-<br style="color: rgb(0, 0, 0); font-family: Arial; font-size: medium; 
-font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; 
-font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; 
-text-indent: 0px; text-transform: none; white-space: normal; widows: 2; 
-word-spacing: 0px; -webkit-text-stroke- text-decoration-thickness: initial; 
+<br style="color: rgb(0, 0, 0); font-family: Arial; font-size: medium;
+font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal;
+font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start;
+text-indent: 0px; text-transform: none; white-space: normal; widows: 2;
+word-spacing: 0px; -webkit-text-stroke- text-decoration-thickness: initial;
 text-decoration-style: initial; text-decoration-color: initial;">
 141 12 STOCKHOLM</p>"""
 FOOTER = """<p>&nbsp;Test</p>"""
