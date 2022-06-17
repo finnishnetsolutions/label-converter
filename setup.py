aa2cc3fef3f3e8ddc83e2789dd448cb09ace0ac2
Julius Leppälä <julius.leppala@nordhealth.com>
2022-06-17 10:10:04 +0300

    Bump to version v0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='label_converter',
-    version='0.14',
+    version='0.15',
     author="Miika Launiainen",
     author_email="miika.launiainen@fns.fi",
     description="HTML to label png converter",
